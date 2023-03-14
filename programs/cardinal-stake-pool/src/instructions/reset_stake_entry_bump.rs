c469344789f873835dea7ab480d3dd166f6da743
Giannis Chatziveroglou <gchatz@mit.edu>
2023-03-14 09:36:40 -0700

    restructure instruction

--- programs/cardinal-stake-pool/src/instructions/reset_stake_entry_bump.rs
+++ programs/cardinal-stake-pool/src/instructions/reset_stake_entry_bump.rs
@@ -1,56 +1,33 @@
 use crate::errors::ErrorCode;
 use crate::state::*;
-use crate::utils::resize_account;
 use anchor_lang::prelude::*;
-
-#[derive(AnchorSerialize, AnchorDeserialize)]
-pub struct ResetStakeEntryBumpIx {
-    bump: u8,
-}
+use anchor_spl::token::Mint;
 
 #[derive(Accounts)]
-#[instruction(ix: ResetStakeEntryBumpIx)]
 pub struct ResetStakeEntryBumpCtx<'info> {
-    #[account(mut)]
     stake_entry: Box<Account<'info, StakeEntry>>,
-
-    #[account(mut, constraint = stake_entry.pool == stake_pool.key() @ ErrorCode::InvalidStakePool)]
-    stake_pool: Box<Account<'info, StakePool>>,
-
-    #[account(mut, constraint = authority.key() == stake_pool.authority @ ErrorCode::InvalidPoolAuthority)]
-    authority: Signer<'info>,
     #[account(mut)]
-    payer: Signer<'info>,
-    system_program: Program<'info, System>,
+    stake_pool: Box<Account<'info, StakePool>>,
+    /// CHECK: This is not dangerous because we don't read or write from this account
+    user: UncheckedAccount<'info>,
+
+    original_mint: Box<Account<'info, Mint>>,
 }
 
-pub fn handler(ctx: Context<ResetStakeEntryBumpCtx>, ix: ResetStakeEntryBumpIx) -> Result<()> {
-    let stake_entry = &mut ctx.accounts.stake_entry;
-    let new_stake_entry = StakeEntry {
-        bump: ix.bump,
-        pool: stake_entry.pool,
-        amount: stake_entry.amount,
-        original_mint: stake_entry.original_mint,
-        original_mint_claimed: stake_entry.original_mint_claimed,
-        last_staker: stake_entry.last_staker,
-        last_staked_at: stake_entry.last_staked_at,
-        total_stake_seconds: stake_entry.total_stake_seconds,
-        stake_mint_claimed: stake_entry.stake_mint_claimed,
-        kind: stake_entry.kind,
-        stake_mint: stake_entry.stake_mint,
-        cooldown_start_seconds: stake_entry.cooldown_start_seconds,
-        last_updated_at: stake_entry.last_updated_at,
-        grouped: stake_entry.grouped,
-    };
-    let new_space = new_stake_entry.try_to_vec()?.len() + 8;
-    stake_entry.set_inner(new_stake_entry);
-
-    resize_account(
-        &stake_entry.to_account_info(),
-        new_space,
-        &ctx.accounts.payer.to_account_info(),
-        &ctx.accounts.system_program.to_account_info(),
-    )?;
+pub fn handler(ctx: Context<ResetStakeEntryBumpCtx>) -> Result<()> {
+    let (pubkey, bump) = Pubkey::find_program_address(
+        &[
+            STAKE_ENTRY_PREFIX.as_bytes(),
+            ctx.accounts.stake_pool.key().as_ref(),
+            ctx.accounts.original_mint.key().as_ref(),
+            get_stake_seed(ctx.accounts.original_mint.supply, ctx.accounts.user.key()).as_ref(),
+        ],
+        ctx.program_id,
+    );
+    ctx.accounts.stake_entry.bump = bump;
+    if pubkey != ctx.accounts.stake_entry.key() {
+        return Err(error!(ErrorCode::InvalidStakeEntry));
+    }
 
     Ok(())
 }
--- programs/cardinal-stake-pool/src/lib.rs
+++ programs/cardinal-stake-pool/src/lib.rs
@@ -93,8 +93,8 @@ pub mod cardinal_stake_pool {
         claim_stake_entry_funds::handler(ctx)
     }
 
-    pub fn reset_stake_entry_bump(ctx: Context<ResetStakeEntryBumpCtx>, ix: ResetStakeEntryBumpIx) -> Result<()> {
-        reset_stake_entry_bump::handler(ctx, ix)
+    pub fn reset_stake_entry_bump(ctx: Context<ResetStakeEntryBumpCtx>) -> Result<()> {
+        reset_stake_entry_bump::handler(ctx)
     }
 
     //// programmable ////
